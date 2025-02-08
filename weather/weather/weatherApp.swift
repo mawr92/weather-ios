004537c748c11fc3f3788edfea1c6b5f7cc9b172
Alejandra Wetsch <alejandra.wetsch@gmail.com>
2025-02-08 16:25:07 -0300

    Initial commit

---
+++ weather/weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Alejandra Wetsch on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ weather/weatherTests/weatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  weatherTests.swift
+//  weatherTests
+//
+//  Created by Alejandra Wetsch on 8/2/25.
+//
+
+import Testing
+@testable import weather
+
+struct weatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
